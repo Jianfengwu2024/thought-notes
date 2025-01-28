4774cdc6ca7ce703a5ece2e9993e3f559be7ceb5
Jianfengwu2024 <muchen.wu@hotmail.com>
2025-01-28 18:02:02 +0800

    check env and start server added

--- generate_project/server.js
+++ generate_project/server.js
@@ -82,7 +82,8 @@ app.post('/generate', (req, res) => {
       'index.html',
       'readme.md',
       'package.json',
-      'start_server.bat'
+      'package_lock.json',
+      'check_env_and_start.py'
     ];
     copyFiles(path.join(__dirname, '../project_template'), projectPath, mainFiles);
 
